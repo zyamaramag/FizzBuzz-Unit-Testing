1ae78d05a7c6dcc223e68aa0f4ad3d66a2d90161
zyamaramag <150358926+zyamaramag@users.noreply.github.com>
2025-04-28 19:52:43 +0800

    Activity code

---
+++ activity.py
@@ -0,0 +1,12 @@
+def fizzbuzz(num):
+    if not isinstance(num, int):
+        return "Not number"
+
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return str(num)
